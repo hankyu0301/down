e15f5997a2e546122b738a361e10056e0d9fee62
Jiwon Park <xoxojw.dev@gmail.com>
2024-02-09 00:46:43 +0900

    feat: ThemeProvider, ModalProvider 추가

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
+import { Open_Sans } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
+import { ModalProvider } from "@/components/providers/ModalProvider";
+import { cn } from "@/lib/utils";
+
+const font = Open_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +19,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ko" suppressHydrationWarning>
+      <body className={cn(font.className, "bg-white")}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+        >
+          <ModalProvider />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
