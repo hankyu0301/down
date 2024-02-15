eff2f6d4bc0fa8c31d7099b0414d11cd9d889e00
Taegyu <59700057+TaegyuHan@users.noreply.github.com>
2024-02-16 00:36:10 +0900

    update : #9 회원가입 Swagger API 실패 케이스 추가

--- backend/src/main/java/com/example/demo/global/auth/OAuth2FailureHandler.java
+++ backend/src/main/java/com/example/demo/global/auth/OAuth2FailureHandler.java
@@ -11,7 +11,6 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Map;
 
 @Slf4j
 @Component
@@ -23,9 +22,7 @@ public class OAuth2FailureHandler implements AuthenticationFailureHandler {
     @Override
     public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
 
-        FailResponse<Map<String, String>> failResponse = FailResponse.<Map<String, String>>builder()
-                .data(Map.of("errorMessage", "로그인 실패 기존에 존재하는 이메일로 가입된 계정이 있습니다."))
-                .build();
+        FailResponse failResponse = FailResponse.of("로그인 실패 기존에 존재하는 이메일로 가입된 계정이 있습니다.");
 
         String stringResponse = objectMapper.writeValueAsString(failResponse);
 
