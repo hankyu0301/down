7ca8d1d77fd99e5cef3906464f200e5656ab4769
Jiwon Park <xoxojw.dev@gmail.com>
2024-02-15 15:47:41 +0900

    feat: #15 네비게이션 바 링크 테스트 기본 라우트 페이지 생성

---
+++ frontend/app/(fleamarket)/(routes)/fleamarket/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FleaMarketPage = () => {
+  return (
+    <div>중고거래 메인 페이지</div>
+  )
+}
+
+export default FleaMarketPage
---
+++ frontend/app/(lesson)/(routes)/lesson/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LessonPage = () => {
+  return (
+    <div>강습 메인 페이지</div>
+  )
+}
+
+export default LessonPage
