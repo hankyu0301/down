6b305d2d9789461f9ef1c04158dd19b67ced083f
Taegyu <59700057+TaegyuHan@users.noreply.github.com>
2024-02-17 18:15:14 +0900

    feat: #36 이메일 코드 일치 버그 수정

--- backend/src/main/java/com/example/demo/domain/user/exception/UserExceptionHandler.java
+++ backend/src/main/java/com/example/demo/domain/user/exception/UserExceptionHandler.java
@@ -4,12 +4,11 @@ package com.example.demo.domain.user.exception;
 import com.example.demo.domain.util.FailResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 
-
-@ControllerAdvice
+@RestControllerAdvice
 public class UserExceptionHandler {
     
     @ExceptionHandler(EmailAlreadyExistsException.class)
@@ -46,6 +45,14 @@ public class UserExceptionHandler {
 
     @ExceptionHandler(EmailVerificationCodeMismatchException.class)
     public ResponseEntity<FailResponse> emailVerificationCodeMismatchExceptionHandler(EmailVerificationCodeMismatchException e) {
+        FailResponse response = FailResponse.of(e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(response);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<FailResponse> illegalAccessExceptionHandler(IllegalArgumentException e) {
         FailResponse response = FailResponse.of(e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
