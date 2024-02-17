1109bca946e7fba47987f86c507d04b0c2d3e0b1
Taegyu <59700057+TaegyuHan@users.noreply.github.com>
2024-02-17 19:27:21 +0900

    feat: #26 아이디 비밀번호 로그인 기능 추가

---
+++ backend/src/main/java/com/example/demo/domain/user/dto/command/UserLoginCommand.java
@@ -0,0 +1,27 @@
+package com.example.demo.domain.user.dto.command;
+
+import com.example.demo.domain.user.model.User;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+@Schema(description = "로그인 요청 DTO")
+public class UserLoginCommand {
+    @Email(message = "이메일 형식이 아닙니다.")
+    @NotBlank(message = "로그인 이메일은 필수 입력 값입니다.")
+    @Schema(description = "로그인 이메일", example = "test@gmai.com")
+    private String email;
+
+    @NotBlank(message = "로그인 비밀번호는 필수 입력 값입니다.")
+    @Schema(description = "로그인 비밀번호", example = "test1234")
+    private String password;
+
+    public User toUserDomain() {
+        return User.builder()
+                .email(email)
+                .password(password)
+                .build();
+    }
+}
