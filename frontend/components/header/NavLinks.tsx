4f7a3c9fbc3ccff5408d6056ecae89b035e8f5ff
Jiwon Park <xoxojw.dev@gmail.com>
2024-03-02 11:52:19 +0900

    feat: #75 드롭다운 메뉴 ui 컴포넌트 추가

--- frontend/components/header/NavLinks.tsx
+++ frontend/components/header/NavLinks.tsx
@@ -3,15 +3,14 @@ import { Button } from "@/components/ui";
 import useProfile from "@/hooks/user/useProfile";
 
 const navLinks = [
-  { label: "모임", href: "/gathering" },
-  { label: "강습", href: "/lesson" },
-  { label: "중고거래", href: "/fleamarket" },
-  { label: "채팅", href: "/chat" },
+	{ label: "모임", href: "/gathering" },
+	{ label: "강습", href: "/lesson" },
+	{ label: "중고거래", href: "/fleamarket" },
+	{ label: "채팅", href: "/chat" },
 ];
 
 const NavLinks = () => {
 	const user = useProfile();
-	console.log("USER!!!", user);
 	return (
 		<ul className="flex gap-6 items-center">
 			{navLinks.map((link) => (
